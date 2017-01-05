6cff893b1df17e6c80f1082f6e2e7f927e6dcec8
lomik248 <lomik248@mail.ru>
2017-01-05 15:09:25 +0300

    new app (MVC_5  with Login)

---
+++ OlxaWeb/OlxaWeb.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OlxaWeb.WebUI.Startup))]
+namespace OlxaWeb.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
