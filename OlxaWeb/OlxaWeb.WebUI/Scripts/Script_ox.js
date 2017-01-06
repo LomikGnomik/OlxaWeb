e7158c9b1816325d317ab49abcbd563676fa3f1b
Unknown <olxaweb@mail.ru>
2017-01-06 22:45:27 +0300

    Добавила новый стиль Layout

---
+++ OlxaWeb/OlxaWeb.WebUI/Scripts/Script_ox.js
@@ -0,0 +1,6 @@
+﻿// Stuck navbar on scroll
+if ($('.navbar-sticky').length && $('.main-navigation').length) {
+    var sticky = new Waypoint.Sticky({
+        element: $('.navbar-sticky .main-navigation')[0]
+    });
+}
