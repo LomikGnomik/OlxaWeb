a8e6956fce8322e4d5f9bee723679bde47655bba
Unknown <olxaweb@mail.ru>
2017-01-07 21:24:13 +0300

    Add new styles & body pages

--- OlxaWeb/OlxaWeb.WebUI/App_Start/RouteConfig.cs
+++ OlxaWeb/OlxaWeb.WebUI/App_Start/RouteConfig.cs
@@ -16,8 +16,13 @@ namespace OlxaWeb.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "Develop",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Develop", action = "Index", id = UrlParameter.Optional });
+          
         }
     }
 }
