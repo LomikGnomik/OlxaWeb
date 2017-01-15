1221e956e20e1d24e87e49f4b86b315f8d239a04
lomik248 <lomik248@mail.ru>
2017-01-16 00:49:12 +0300

    Добавил таблицу WebSite

---
+++ OlxaWeb/OlxaWeb.Domain/Concrete/EFWebSiteRepository.cs
@@ -0,0 +1,20 @@
+﻿using OlxaWeb.Domain.Abstract;
+using OlxaWeb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlxaWeb.Domain.Concrete
+{
+   public class EFWebSiteRepository : IWebSiteRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<WebSite> WebSites
+        {
+            get { return context.WebSites; }
+        }
+    }
+}
